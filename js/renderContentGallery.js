8a2e0a2ccb598a2be5049b1d479b55f84e4d40f9
eimuc <43643903+eimuc@users.noreply.github.com>
2020-10-05 22:05:59 +0300

    Gallery

--- js/renderContentGallery.js
+++ js/renderContentGallery.js
@@ -1,6 +1,34 @@
-function renderContentGallery() {
-    return `<div class="gallery">
-                IMAGES
+function renderContentGallery(photoArray) {
+    // validacijos
+    // jei nera duotas nuotrauku sarasas
+    if (!photoArray) {
+        return '';
+    }
+
+    // logika
+    const maxGallerySize = 3;
+    const printablePhotosCount = photoArray.length > maxGallerySize ? maxGallerySize : photoArray.length;
+
+    // generuojame tik reikiama kieki nuotrauku
+    let extraHTML = '';
+    if (photoArray.length > maxGallerySize) {
+        extraHTML = `data-extra="${photoArray.length - maxGallerySize}"`;
+    }
+
+    let photosHTML = '';
+    for (let i = 0; i < printablePhotosCount; i++) {
+        if (i < printablePhotosCount - 1) {
+            photosHTML += `<img src="./img/posts/${photoArray[i]}"
+                                alt="User post gallery picture">`;
+        } else {
+            photosHTML += `<img src="./img/posts/${photoArray[i]}"
+                                alt="User post gallery picture"
+                                ${extraHTML}>`;
+        }
+    }
+
+    return `<div class="gallery gallery-${printablePhotosCount}">
+                ${photosHTML}
             </div>`;
 }
 
