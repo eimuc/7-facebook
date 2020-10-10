00142d6b2c07856881b6f77de837b3b3c6b14b4c
eimuc <43643903+eimuc@users.noreply.github.com>
2020-10-10 23:37:50 +0300

    final version

--- js/renderContent.js
+++ js/renderContent.js
@@ -1,9 +1,15 @@
 import renderContentText from './renderContentText.js';
 import renderContentGallery from './renderContentGallery.js';
 
-function renderContent(data) {
-    return `<div>
-                ${renderContentText(data.text)}
+function renderContent(data, post_id) {
+    // console.log(data);
+    const contentText = {
+        id: post_id,
+        text: data.text,
+        background: data.background
+    };
+    return `<div class="content">
+                ${renderContentText(contentText)}
                 ${renderContentGallery(data.photos)}
             </div>`;
 }
